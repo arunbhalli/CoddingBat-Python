2fbbf9d26ca0d24f300da1d0daae9e4cd1ac2a9f
arunbhalli <tomarbhalli@gmail.com>
2022-09-19 10:32:43 +0200

    adds solution for missing character case

---
+++ warmup-1/missing_char.py
@@ -0,0 +1,27 @@
+'''
+
+Given a non-empty string and an int n, return a new string where the char at index n has been removed. The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..len(str)-1 inclusive).
+
+
+missing_char('kitten', 1) → 'ktten'
+missing_char('kitten', 0) → 'itten'
+missing_char('kitten', 4) → 'kittn'
+
+'''
+str = 'kitten'
+n = len(str)
+for n in range(0,len(str)-1):
+ front = str[:n]
+ back = str[n+1:]
+print(front+back)
+    
+'''
+print(str)
+print(str[0])
+print(str[0:n:2])
+print(str.replace(str[0],''))
+
+'''
+
+
+  
