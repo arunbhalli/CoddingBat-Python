6bc14ba6ee1e913ddc79539cc82d4e62d4edc12f
arunbhalli <tomarbhalli@gmail.com>
2022-10-18 09:52:28 +0200

    adds solution of make out word

--- string1/make_abba.py
+++ string1/make_abba.py
@@ -9,18 +9,8 @@ make_abba('What', 'Up') → 'WhatUpUpWhat'
 
 def make_abba(a, b):
   result = a+b+b+a
-  return result
+  return print( result)
+make_abba('Hi', 'Bye')
+make_abba('Yo', 'Alice')
+make_abba('What', 'Up')
 
-'''
-The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text. In this example, the "i" tag makes <i> and </i> which surround the word "Yay". Given tag and word strings, create the HTML string with tags around the word, e.g.
- "<i>Yay</i>".
-make_tags('i', 'Yay') → '<i>Yay</i>'
-make_tags('i', 'Hello') → '<i>Hello</i>'
-make_tags('cite', 'Yay') → '<cite>Yay</cite>'
-
-'''
-
-'''
-def make_tags(tag, word):
-  return ('<'+tag+'>'+ word+'<'+'/'+tag+'>')
-'''
